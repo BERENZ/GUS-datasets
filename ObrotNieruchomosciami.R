b9a6faa84a45e0e7f99979ec680e4e67d7b8ff2e
Maciej Beręsewicz <BERENZ@users.noreply.github.com>
2016-04-26 19:46:00 +0200

    Update ObrotNieruchomosciami.R

--- ObrotNieruchomosciami.R
+++ ObrotNieruchomosciami.R
@@ -1,12 +1,12 @@
 library(openxlsx)
 setwd('~/Documents/Uczelnia/Zbiory/GUS/obrót nieruchomosciami/obrot nieruchomosciami/')
-pliczki <- list.files()
+pliczki <- list.files('*.xlsx')
 
 # 2014 --------------------------------------------------------------------
 
-f2014 <- pliczki[grepl('2014',pliczki)]
+f2014 <- pliczki[grepl('2014|2013',pliczki)]
 
-obrot2014kraj <- lapply(
+obrot2013_14_kraj <- lapply(
   f2014,
   read.xlsx,
   sheet = 3,
@@ -14,7 +14,8 @@ obrot2014kraj <- lapply(
   colNames = F
 )
 
-obrot2014woj <- lapply(
+
+obrot2013_14_woj <- lapply(
   f2014,
   read.xlsx,
   sheet = 4,
@@ -22,7 +23,7 @@ obrot2014woj <- lapply(
   colNames = F
 )
 
-obrot2014pow <- lapply(
+obrot2013_14_pow <- lapply(
   f2014,
   read.xlsx,
   sheet = 5,
@@ -30,7 +31,51 @@ obrot2014pow <- lapply(
   colNames = F
 )
 
-# 2013 --------------------------------------------------------------------
+names(obrot2013_14_kraj) <- 
+  names(obrot2013_14_woj) <- 
+  names(obrot2013_14_pow) <- f2014
+
+# ealier --------------------------------------------------------------------
+
+obrot2012i <- read.xlsx(xlsxFile = 'T1_2012_I_pol.xlsx',
+                       start = 7,
+                       sheet = 'Polska',
+                       colNames = F)
+
+obrot2012ii <- read.xlsx(xlsxFile = 'T1_2012_II_pol.xlsx',
+                       start = 7,
+                       sheet = 'Polska',
+                       colNames = F)
+
+obrot2012iwoj <- read.xlsx(xlsxFile = 'T1_2012_I_pol.xlsx',
+                        start = 6,
+                        sheet = 'województwa',
+                        colNames = F)
+
+obrot2012iiwoj <- read.xlsx(xlsxFile = 'T1_2012_II_pol.xlsx',
+                         start = 6,
+                         sheet = 'województwa',
+                         colNames = F)
 
 
 
+
+# save all ----------------------------------------------------------------
+
+
+obrot <-
+  list(
+    obrot2012kraj = list(
+      'T1_2012_I_pol.xlsx' = obrot2012i,
+      'T1_2012_II_pol.xlsx' = obrot2012ii
+    ),
+    obrot2012woj = list(
+      'T1_2012_I_pol.xlsx' = obrot2012iwoj,
+      'T1_2012_II_pol.xlsx' = obrot2012iiwoj
+    ),
+    obrot2013_14_kraj,
+    obrot2013_14_woj,
+    obrot2013_14_pow
+  )
+
+save(obrot,file='obrot.rda')
